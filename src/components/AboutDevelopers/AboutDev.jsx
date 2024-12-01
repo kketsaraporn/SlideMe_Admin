79e4f052b7187c46c776f8f263c6883495ca142d
nopparatwww <nopparat.suw@spumail.net>
2024-12-01 15:30:06 +0700

    UpdateAboutDev

---
+++ src/components/AboutDevelopers/AboutDev.jsx
@@ -0,0 +1,11 @@
+import "./AboutDev.css";
+
+function AboutDev() {
+    return (
+        <div className="about-dev">
+            <p>Developers</p>
+        </div>
+    );
+}
+
+export default AboutDev;
